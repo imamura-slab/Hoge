d697d095fc0ead7b9676de88f4d145532dc03cfb
saikai-slab <s.taichi1997@gmail.com>
2020-08-17 22:35:24 +0900

    Add overload operator

---
+++ cpp/beginners_guide/overload/src/overload3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Hoge
+{
+private:
+  const char *str;
+  
+public:
+  Hoge() { cout << "だが断る" << endl; }         // デフォルトコンストラクタ
+  Hoge(const char *str) { cout << str << endl; } // コンストラクタのオーバーロード
+
+  /* 演算子 + のオーバーロード */
+  char* operator+(Hoge rhs)
+  {
+    return "ナニッ！？"; // 引数使え警告でるけど無視
+  } 
+};
+
+int main(void)
+{
+  Hoge hoge1;
+  Hoge hoge2("でも断る");
+  cout << hoge1 + hoge2 << endl; // Hogeクラス + Hogeクラス
+  return 0;
+}
