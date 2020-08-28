0755dc2d921cd8c40972c08a95ee6d7ce383b7dd
saikai-slab <s.taichi1997@gmail.com>
2020-08-28 20:39:14 +0900

    Add libstd++/container/vector

--- cpp/libstdc++/container/README.md
+++ cpp/libstdc++/container/README.md
@@ -12,7 +12,7 @@
 - プログラム例
    - [ソースコード](src/array.cpp)
    - 出力
-     ```
+     ```sh
      $ g++ -std=c++11 array.cpp && ./a.out 
      array size: 4
      1 2 3 4
@@ -26,4 +26,40 @@
      3.14 3.14
      
      $ 
+     ```
+
+## 可変長配列
+- `<vector>`ヘッダをインクルード
+- コンストラクト
+  ```cpp
+  std::vector<[型やクラス]> v;
+  std::vector<[型やクラス]> v([要素数]);
+  std::vector<[型やクラス]> v([要素数], [初期値]);
+  std::vector<std::vector<[型やクラス]> > v([行], std::vector<[型やクラス]>, [列]);
+  ...
+  ```
+- メソッド
+  - `.size()`: 配列サイズを取得
+  - `.pushback()`: 末尾へ要素を追加 
+  - `.empty()`: 空かどうかを判定
+  - `.resize()`: 要素数のリサイズ
+  - などなど
+
+- プログラム例
+   - [ソースコード](src/vector.cpp)
+   - 出力
+     ```sh
+     $ g++ -std=c++11 vector.cpp && ./a.out
+     0 1 2 3 4 5 
+     poped 5 -> poped 4 -> poped 3 -> poped 2 -> poped 1 -> poped 0 -> empty!
+     hoge
+     huga
+     piyo
+     9 9 9 9 9 9 9 
+     9 9 9 9 9 9 9 
+     9 9 9 9 9 9 9 
+     9 9 9 9 9 9 9 
+     9 9 9 9 9 9 9 
+
+     $
      ```
---
+++ cpp/libstdc++/container/src/vector.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+  /* int 型の可変長配列 */
+  std::vector<int> v = {0, 1, 2};
+
+  v.push_back(3); // のびる
+  v.push_back(4); // のびる
+  v.push_back(5); // どこまでも
+  for (int i = 0; i < v.size(); i++) { // .size()でサイズ取得
+    std::cout << v.at(i) << " "; // .at()メソッド／[]演算子で参照
+  }
+  std::cout << std::endl;
+
+  /* 空になるまで pop */
+  while (!v.empty()) {
+    std::cout << "poped " << v.back() << " -> ";
+    v.pop_back();
+  }
+  std::cout << "empty!" << std::endl;
+  
+  /* string 型でも構造体でもクラスでも */
+  std::vector<std::string> s = {"hoge", "huga", "piyo"};
+  for (std::string e : s) {      // | 範囲ベース for 文
+    std::cout << e << std::endl; // | を使ったアクセスの
+  }                              // | 記述（eはelementの意）
+  
+  /* 2次元の可変長配列 */
+  std::vector<std::vector<int> > vv(5, std::vector<int>(7, 9)); // 5x7の2次元配列
+                                                                // を5で初期化
+  // std::vector<std::vector<float>> vv(); 
+  //                              ~~ コンパイラのバージョンによってはエラー
+  for (std::vector<int> ve : vv) {
+    for (int e : ve) {
+      std::cout << e << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+  
+  return 0;
+}
